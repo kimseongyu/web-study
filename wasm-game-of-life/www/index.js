650454bba26cfb448b3ecd25a9e294a4bb4ecbff
kimseongyu <kskksk0925@naver.com>
2025-11-28 16:29:23 +0900

    add fps tracking and optimization wasm-game-of-life

--- wasm-game-of-life/www/index.js
+++ wasm-game-of-life/www/index.js
@@ -1,5 +1,43 @@
 import init, { Universe, Cell } from "wasm-game-of-life";
 
+const fps = new (class {
+  constructor() {
+    this.fps = document.getElementById("fps");
+    this.frames = [];
+    this.lastFrameTimeStamp = performance.now();
+  }
+
+  render() {
+    const now = performance.now();
+    const delta = now - this.lastFrameTimeStamp;
+    this.lastFrameTimeStamp = now;
+    const fps = (1 / delta) * 1000;
+
+    this.frames.push(fps);
+    if (this.frames.length > 100) {
+      this.frames.shift();
+    }
+
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    for (let i = 0; i < this.frames.length; i++) {
+      sum += this.frames[i];
+      min = Math.min(this.frames[i], min);
+      max = Math.max(this.frames[i], max);
+    }
+    let mean = sum / this.frames.length;
+
+    this.fps.textContent = `
+Frames per Second:
+         latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+`.trim();
+  }
+})();
+
 const start = async () => {
   const wasm = await init();
 
@@ -45,11 +83,31 @@ const start = async () => {
 
     ctx.beginPath();
 
+    ctx.fillStyle = ALIVE_COLOR;
     for (let row = 0; row < height; row++) {
       for (let col = 0; col < width; col++) {
         const idx = getIndex(row, col);
+        if (cells[idx] !== Cell.Alive) {
+          continue;
+        }
 
-        ctx.fillStyle = cells[idx] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
+        ctx.fillRect(
+          col * (CELL_SIZE + 1) + 1,
+          row * (CELL_SIZE + 1) + 1,
+          CELL_SIZE,
+          CELL_SIZE
+        );
+      }
+    }
+
+    ctx.fillStyle = DEAD_COLOR;
+    for (let row = 0; row < height; row++) {
+      for (let col = 0; col < width; col++) {
+        const idx = getIndex(row, col);
+        if (cells[idx] !== Cell.Dead) {
+          continue;
+        }
+
         ctx.fillRect(
           col * (CELL_SIZE + 1) + 1,
           row * (CELL_SIZE + 1) + 1,
@@ -86,8 +144,11 @@ const start = async () => {
   let animationId = null;
 
   const renderLoop = () => {
-    universe.tick();
+    fps.render();
 
+    for (let i = 0; i < 1000; i++) {
+      universe.tick();
+    }
     drawGrid();
     drawCells();
 
