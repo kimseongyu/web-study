d03c564f89691ae2e88bdd19ad6f0ee7f48ec718
kimseongyu <kskksk0925@naver.com>
2025-12-03 17:39:32 +0900

    add rendering line with three.js

--- three/main.js
+++ three/main.js
@@ -1,29 +1,42 @@
 import * as THREE from "three";
-import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
 
+// Scene
 const scene = new THREE.Scene();
+
 // Perspective camera
 // fov(field of view): angle of view
 // aspect(aspect ratio): width / height
 // near(near clipping plane): distance from the camera to the near clipping plane
 // far(far clipping plane): distance from the camera to the far clipping plane
 const camera = new THREE.PerspectiveCamera(
-  75,
+  45,
   window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+  1,
+  500
 );
+camera.position.set(0, 0, 30);
+camera.lookAt(0, 0, 0);
 
+// Renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Geometry
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+// Line
+const line_material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+const points = [];
+points.push(new THREE.Vector3(-10, 0, 0));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, 0, 0));
+const line_geometry = new THREE.BufferGeometry().setFromPoints(points);
+const line = new THREE.Line(line_geometry, line_material);
+scene.add(line);
 
 function animate() {
   cube.rotation.x += 0.01;
